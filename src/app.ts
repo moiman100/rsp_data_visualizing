6e50db8d277efdecdf0d2429c55b276f4c1d672d
akaruin <akaruin2@gmail.com>
2020-02-13 19:57:42 +0200

    added ejs view engine, small project structure additions, added home view

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+// serve public directory
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.render("index", {title: "Home"});
 })
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
      console.log(`Server is running in http://localhost:${PORT}`)
