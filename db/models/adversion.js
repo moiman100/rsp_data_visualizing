8c68ad6094d24e9c574f5829f179cbb509db1b5d
Joonas Hakkarainen <joonas.hakkarainen@hotmail.com>
2020-02-18 12:57:29 +0000

    Added db schemas/models and a db test program

---
+++ db/models/adversion.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var Ad = require("./ad");
+
+var Schema = mongoose.Schema;
+
+var AdVersionsSchema = new Schema({
+    ad: { type: Schema.Types.ObjectId, ref: "Ad" },
+    version_name: String,
+    release_date: { type: Date, default: Date.now },
+    language: String
+});
+
+module.exports = mongoose.model("AdVersion", AdVersionsSchema);
