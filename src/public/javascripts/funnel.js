39794e481f5e73abe3290006a27342a22e1421f2
Mikko Mustonen <mustonen.mikko@outlook.com>
2020-03-19 00:25:29 +0200

    Refactored and buggy

--- src/public/javascripts/funnel.js
+++ src/public/javascripts/funnel.js
@@ -1,12 +1,15 @@
 "use strict";
 const {
   Select,
+  NativeSelect,
   MenuItem,
   InputLabel,
   FormControl,
   makeStyles,
   Container,
   Button,
+  Card,
+  Box,
 } = MaterialUI;
 
 const { useState, useEffect } = React;
@@ -31,6 +34,8 @@ function FunnelGraphf(props) {
   const [ad_version, setAd_version] = useState("");
   const [event_flow, setEvent_flow] = useState([]);
 
+  const classes = useStyles();
+
   function handleChange(index) {
     setEvent_flow(event_flow.splice(index, 1, event.target.value));
   }
@@ -40,7 +45,7 @@ function FunnelGraphf(props) {
   }
 
   function handleAdd() {
-    setEvent_flow([...prevState.event_flow, ""]);
+    setEvent_flow([...event_flow, ""]);
   }
 
   function getAds() {
@@ -65,14 +70,14 @@ function FunnelGraphf(props) {
   // demo function
   function fibonacci() {
     let fibonacci_list = [0, 1];
-    while (fibonacci_list.length < this.state.event_flow.length) {
+    while (fibonacci_list.length < event_flow.length) {
       let index = fibonacci_list.length;
       fibonacci_list.push(
         fibonacci_list[index - 2] + fibonacci_list[index - 1]
       );
     }
     fibonacci_list = fibonacci_list.reverse();
-    return fibonacci_list.slice(0, this.state.event_flow.length);
+    return fibonacci_list.slice(0, event_flow.length);
   }
 
   function getData() {
@@ -83,7 +88,7 @@ function FunnelGraphf(props) {
       fill: "tozeroy",
       type: "scatter",
     };
-    data_object.y = this.fibonacci(); // from API
+    data_object.y = fibonacci(); // from API
     let data_object_list = [data_object];
     setGraph_data(data_object_list);
   }
@@ -97,178 +102,54 @@ function FunnelGraphf(props) {
   }, []);
 
   return (
+    // Could be divided into smaller components
     <Container maxWidth="sm">
-      <FormControl>
+      <FormControl className={classes.formControl}>
         <InputLabel>Ad</InputLabel>
-        <Select defaultValue="" onChange={getAdVersions}>
+        <NativeSelect defaultValue="" onChange={getAdVersions}>
           <OptionList inputList={available_ads} />
-        </Select>
+        </NativeSelect>
       </FormControl>
-      <FormControl>
+      <FormControl className={classes.formControl}>
         <InputLabel>Version</InputLabel>
-        <Select defaultValue="" onChange={getEvents}>
+        <NativeSelect defaultValue="" onChange={getEvents}>
           <OptionList inputList={available_ad_versions} />
-        </Select>
+        </NativeSelect>
       </FormControl>
+      <Box>
+        <Button onClick={handleAdd}>Add new event</Button>
+        <Button onClick={getData}>Update funnel</Button>
+      </Box>
+      <Card id="funnel_graph"></Card>
       {event_flow.map((selected_event, index) => (
-        <div key={index}>
-          <Select
+        <FormControl key={index} className={classes.formControl}>
+          <InputLabel>Event</InputLabel>
+          <NativeSelect
             value={event_flow[index]}
             onChange={handleChange.bind(this, index)}
           >
-            <option disabled value="">
-              Event
-            </option>
             <OptionList inputList={available_events} />
-          </Select>
+          </NativeSelect>
           <Button onClick={handleRemove.bind(this, index)}>Remove</Button>
-        </div>
+        </FormControl>
       ))}
-      <Button onClick={handleAdd.bind(this)}>Add new event</Button>
-      <Button onClick={getData.bind(this)}>Update funnel</Button>
     </Container>
   );
 }
 
+// This doesn't work in Select for some reason so using NativeSelect for now
 function OptionList(props) {
   return (
     <React.Fragment>
+      <option aria-label="None" value="" />
       {props.inputList.map((name, index) => (
-        <MenuItem key={index} value={name}>
+        <option key={index} value={name}>
           {name}
-        </MenuItem>
+        </option>
       ))}
     </React.Fragment>
   );
 }
 
-class FunnelGraph extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      graph_data: [],
-      available_ads: [],
-      available_ad_versions: [],
-      available_events: [],
-      ad: "",
-      ad_version: "",
-      event_flow: [],
-    };
-  }
-
-  componentDidMount() {
-    this.getAds();
-  }
-
-  handleChange(index) {
-    let list_copy = [...this.state.event_flow];
-    list_copy.splice(index, 1, event.target.value);
-    this.setState({ event_flow: list_copy });
-  }
-
-  handleRemove(index) {
-    let list_copy = [...this.state.event_flow];
-    list_copy.splice(index, 1);
-    this.setState({ event_flow: list_copy });
-  }
-
-  handleAdd() {
-    this.setState(prevState => ({ event_flow: [...prevState.event_flow, ""] }));
-  }
-
-  getAds() {
-    // some API call to get a list of ads
-    let ad_list = ["ad1", "ad2", "ad3"];
-    this.setState({ available_ads: ad_list });
-  }
-
-  getAdVersions() {
-    // some API call to get a list of ad versions
-    let version_list = ["1.0", "0.5"];
-    this.setState({ available_ad_versions: version_list });
-  }
-
-  getEvents() {
-    // some API call to get a list of events
-    let event_list = ["test", "case", "move", "press", "click"];
-    this.setState({ available_events: event_list });
-  }
-
-  // demo function
-  fibonacci() {
-    let fibonacci_list = [0, 1];
-    while (fibonacci_list.length < this.state.event_flow.length) {
-      let index = fibonacci_list.length;
-      fibonacci_list.push(
-        fibonacci_list[index - 2] + fibonacci_list[index - 1]
-      );
-    }
-    fibonacci_list = fibonacci_list.reverse();
-    return fibonacci_list.slice(0, this.state.event_flow.length);
-  }
-
-  getData() {
-    // some API call to get data for the graph
-    // call should send ad, ad_version and event_flow to server
-    let data_object = {
-      y: [],
-      fill: "tozeroy",
-      type: "scatter",
-    };
-    data_object.y = this.fibonacci(); // from API
-    let data_object_list = [data_object];
-    this.setState({ graph_data: data_object_list });
-  }
-
-  render() {
-    Plotly.react("funnel_graph", this.state.graph_data);
-    return (
-      <div>
-        <FormControl>
-          <InputLabel>Ad</InputLabel>
-          <Select defaultValue="" onChange={this.getAdVersions.bind(this)}>
-            {this.state.available_ads.map((ad_name, index) => (
-              <MenuItem key={index} value={ad_name}>
-                {ad_name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel>Version</InputLabel>
-          <Select defaultValue="" onChange={this.getEvents.bind(this)}>
-            {this.state.available_ad_versions.map((ad_version, index) => (
-              <MenuItem key={index} value={ad_version}>
-                {ad_version}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        {this.state.event_flow.map((selected_event, index) => (
-          <div key={index}>
-            <select
-              value={this.state.event_flow[index]}
-              onChange={this.handleChange.bind(this, index)}
-            >
-              <option disabled value="">
-                Event
-              </option>
-              {this.state.available_events.map((event_name, index) => (
-                <option key={index} value={event_name}>
-                  {event_name}
-                </option>
-              ))}
-            </select>
-            <button onClick={this.handleRemove.bind(this, index)}>
-              Remove
-            </button>
-          </div>
-        ))}
-        <button onClick={this.handleAdd.bind(this)}>Add new event</button>
-        <button onClick={this.getData.bind(this)}>Update funnel</button>
-      </div>
-    );
-  }
-}
 const domContainer = document.querySelector("#selectors");
 ReactDOM.render(<FunnelGraphf />, domContainer);
