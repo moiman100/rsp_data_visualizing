d697e3176ee7193c513ad45bd0ddd279dbd66c11
Joonas Hakkarainen <joonas.hakkarainen@hotmail.com>
2020-04-07 16:46:51 +0000

    Added event-types to ad version

--- src/models/adversion.js
+++ src/models/adversion.js
@@ -9,7 +9,8 @@ var AdVersionsSchema = new Schema({
     ad: { type: Schema.Types.ObjectId, ref: "Ad" },
     version_name: String,
     release_date: { type: Date, default: Date.now },
-    language: String
+    language: String,
+    event_types: [String]
 });
 
 module.exports = mongoose.model("AdVersion", AdVersionsSchema);
